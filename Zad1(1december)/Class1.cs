8736579d90f8105ae88f1890804a7a767c125f80
k.chukharev@mail.ru <k.chukharev@mail.ru>
2020-12-07 16:25:25 +0300

    Добавьте файлы проекта.

---
+++ Zad1(1december)/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1_1december_
+{
+    class Class1
+    {
+        int per1 = 5;
+        int per2 = 50;
+        public void Out()
+        {
+            System.Windows.MessageBox.Show($"Новые значения: {per1}, {per2}");
+        }
+        public void Change(int oneArgue, int twoArgue)
+        {
+            per1 = oneArgue;
+            per2 = twoArgue;
+        }
+        int Sum()
+        {
+            int sum = per1 + per2;
+            return sum;
+        }
+        int Greatest()
+        {
+            return per1 > per2 ? per1 : per2;
+        }
+    }
+}
---
+++ Zad1(1december)/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zad1_1december_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Class1 one = new Class1();
+        private void button_click(object sender, RoutedEventArgs e)
+        {
+            string[] strings = textBox.Text.Split(' ');
+            int[] ints = new int[strings.Length];
+            {
+                int iteration = 0;
+                foreach (string g in strings)
+                {
+                    ints[iteration] = Convert.ToInt32(strings[iteration]);
+                    iteration++;
+                }
+            }
+            one.Change(ints[0], ints[1]);
+            one.Out();
+        }
+    }
+}
